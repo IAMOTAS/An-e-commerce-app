9adc07f9d5331cf60fe4f4a400f47a255025931d
IAMOTAS <niiotasalokome@gmail.com>
2023-05-02 10:27:45 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace An_e_commerce_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
